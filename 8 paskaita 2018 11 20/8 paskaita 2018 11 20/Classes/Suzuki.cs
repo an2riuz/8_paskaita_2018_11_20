0da3ee9042d6dc3e1c684861ce620886a8a15d0e
an2riuz <an2riuz@gmail.com>
2018-11-21 20:56:03 +0200

    Creating classes.

---
+++ 8 paskaita 2018 11 20/8 paskaita 2018 11 20/Classes/Suzuki.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_paskaita_2018_11_20.Classes
+{
+    class Suzuki
+    {
+        // duomenys
+        public string Model;
+        private int Year;
+        public string Color;
+        private bool Immobilizer;
+
+        // konstruktorius
+        public Suzuki(string bikeModel, int yearMade, string mainColor, bool hasImmobilizer)
+        {
+            Model = bikeModel;
+            Year = yearMade;
+            Color = mainColor;
+            Immobilizer = hasImmobilizer;
+        }
+
+
+        //metodai
+        public void StartingEngine()
+        {
+
+        }
+
+        private void HowToWheelie()
+        {
+
+        }
+    }
+}
--- 8 paskaita 2018 11 20/8 paskaita 2018 11 20/Program.cs
+++ 8 paskaita 2018 11 20/8 paskaita 2018 11 20/Program.cs
@@ -7,14 +7,7 @@ namespace _8_paskaita_2018_11_20
     {
         static void Main(string[] args)
         {
-            // Pakartokime list
-            List<string> zodziai = new List<string>() {"vienas", "du", "trys"};
 
-            zodziai.Add("testas");
-            zodziai.Insert(2, "kart du");
-
-            Console.WriteLine(" " + zodziai);
-            Console.ReadKey();
         }
     }
 }
