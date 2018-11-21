94ea1c8750c052495071ccf1eeb55077ebe8a6d1
an2riuz <an2riuz@gmail.com>
2018-11-21 18:08:07 +0200

    Pradzia.

---
+++ 8 paskaita 2018 11 20/8 paskaita 2018 11 20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _8_paskaita_2018_11_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
